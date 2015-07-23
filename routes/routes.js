781418e3206653d222ed1829480ae5c6d8a40f7e
Daniel Leary <daniel.leary14@imperial.ac.uk>
2015-07-23 18:57:34 +0100

    added sequelize boilerplate

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,8 @@
+var models  = require('../models');
 var express = require('express');
 var router = express.Router();
 
-var offlineDb = require('../models/offlineDb')
+var offlineDb = require('../models2/offlineDb')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
