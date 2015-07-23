ec24d86211ed0f985a56b777e415ad3f087806b7
Daniel Leary <daniel.leary14@imperial.ac.uk>
2015-07-23 21:01:39 +0100

    second story db test on heroku

--- routes/routes.js
+++ routes/routes.js
@@ -13,18 +13,18 @@ router.get('/', function(req, res, next) {
 router.get('/createstory', function(req, res, next) {
 	models.Story.create({ 
 		url:'/', 
-		title:'Hiking the Appalachian trail: How I quit my consulting job and never looked back.', 
-		text:'Test',
-		points:12, 
-		comments:24, 
+		title:'How a Tokyo bookstore made me fall back in love with print.', 
+		text:'More test',
+		points:90, 
+		comments:21, 
 		commentUrl:'/',
 		date: new Date(),
-		authorName: 'Chris Stewart',
+		authorName: 'Jenny Cross',
 		authorLink: '/',
-		happy: 10,
-		funny: 12,
+		happy: 3,
+		funny:6,
 		sad: 0,
-		angry: 8 
+		angry: 0
 	}).then(function(story) {
   		console.log('Logging item: ' + story.get({plain: true}))
 	})
